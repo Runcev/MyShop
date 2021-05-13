b0c01f1afa85627bd2f20966746ff417dbb13894
Vitya <vitalii.kenyiz@ukma.edu.ua>
2021-05-13 13:44:14 +0300

    Initial commit

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { login } from '../actions/userActions'
+import axios from "axios";
+
+const nodemailer = require("nodemailer");
+
+async function main() {
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'vitalik.trick2001@gmail.com',
+      pass: '17052001',
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  })
+
+  let mailOptions = {
+    from: 'vitalik.trick2001@gmail.com', // sender address
+    to: "vitalii.kenyiz@ukma.edu.ua", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  }
+
+  // send mail with defined transport object
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.message);
+  })
+
+}
+
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    dispatch(login(email, password))
+    await main()
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const dataSubmit = {
+      email
+    }
+
+    axios.post("api/sendMail", dataSubmit)
+  }
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button type='submit'  onClick={handleSubmit} variant='primary'>
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
